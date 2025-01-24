b520205e12b81094110014e18f6d40a26098c234
Peng Xiao <pengxiao@outlook.com>
2025-01-25 00:38:54 +0800

    chore: publish v2 files

--- packages/affine-reader/esbuild.config.js
+++ packages/affine-reader/esbuild.config.js
@@ -1,7 +1,7 @@
 import { build } from "esbuild";
 
 build({
-  entryPoints: ["./index.ts", "./blog.ts", "./template.ts"],
+  entryPoints: ["./index.ts", "./blog.ts", "./template.ts", "./template-v2.ts"],
   bundle: true,
   platform: "node",
   outdir: "dist",
